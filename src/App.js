ea171659de729ea10a7ffbe355095b27558667b2
harshit0413 <137285674+harshit0413@users.noreply.github.com>
2023-10-17 02:49:52 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo.png';
 import './App.css';
 import { BrowserRouter, Routes, Route, useNavigate } from 'react-router-dom';
 import Login from './Pages/Login';
