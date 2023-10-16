d835db44574057dcabcee1d1a2ccb04aa5cc22ea
harshit0413 <137285674+harshit0413@users.noreply.github.com>
2023-10-17 02:57:30 +0530

    Header.js

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -20,7 +20,7 @@ function Header() {
   return (
     <nav class="navbar navbar-expand-lg navbar-light ">
         <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarTogglerDemo01">
-            <a class="navbar-brand main-heading" style={{fontSize: '24px'}}>ROJGAR <img className='vector ml-2' src={require('../assets/logo.png')} /></a>
+            <a class="navbar-brand main-heading" style={{fontSize: '24px'}}>ROJGAR <img className='vector ml-2' src={require('../assets/log.png')} /></a>
             <ul class="navbar-nav  mt-2 mt-lg-0">
             <li class="nav-item mr-lg-3">
                 <Link to='/'><a class="nav-link" style={{fontSize: '20px'}} href="#">Home <span class="sr-only">(current)</span></a></Link>
