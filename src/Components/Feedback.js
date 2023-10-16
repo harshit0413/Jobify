0b503e9d6aae6d7c8596430fcdeb615f41488bbd
harshit0413 <137285674+harshit0413@users.noreply.github.com>
2023-10-17 02:59:38 +0530

    Feedback.js

--- src/Components/Feedback.js
+++ src/Components/Feedback.js
@@ -9,7 +9,7 @@ function Feedback() {
         <div className='py-3 feedbackDiv mx-auto d-flex flex-column align-items-center justify-content-center'>
             <p className='happy'>Team Rojgar</p>
             <img style={{width: '22em'}} src={require('../assets/CEO.png')} />
-            <p className='mb-0' style={{maxWidth: '500px',lineHeight:'1em'}}>
+            <p className='mb-0' style={{maxWidth: '250px',lineHeight:'2em'}}>
             Welcome to ROJGAR - your gateway to a world of opportunities. We're dedicated to helping you find the perfect job that aligns with your skills and aspirations. With a user-friendly interface, an extensive job database, and valuable career resources, we're here to support your career journey. Feel safe and supported with our strong commitment to privacy and a helpful community. Join us at ROJGAR and let's make your career dreams a reality. Welcome to a world of endless possibilities.
         </div>
     </div>
